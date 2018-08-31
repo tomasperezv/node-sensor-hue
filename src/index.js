f5e6332fe6440f16c0ff68f8115951890eef1c0a
Tomás Pérez <t@0x101.com>
2018-08-31 20:35:47 +0200

    Reading sensors.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ bridgeManager.get()
     const client = new huejay.Client({ host: bridge.ip, username: process.env.HUE_USERNAME });
 
     client.sensors.getAll()
-      .then(sensors => {
-        sensors.forEach((sensor) => console.log(sensor.uniqueId)); // eslint-disable-line
+      .then((sensors) => {
+        const map = {};
+        sensors.forEach((sensor) => {
+          if (typeof sensor.uniqueId === 'undefined') {
+            return;
+          }
+          //console.log(JSON.stringify(sensor));
+
+          if (typeof map[sensor.name] !== 'undefined') {
+            map[sensor.name] = Object.assign(map[sensor.uniqueId], sensor);
+          } else {
+            map[sensor.name] = sensor;
+          }
+        });
+
+        console.log(JSON.stringify(map['Office sensor'])); // eslint-disable-line
       });
   });
