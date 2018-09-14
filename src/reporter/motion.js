e26b476807d8be434b751950b23fb9c3cb8e5c5c
Tomás Pérez <t@0x101.com>
2018-09-14 09:44:12 +0200

    Incorporating sensor id as a part of the metric identifier.

--- src/reporter/motion.js
+++ src/reporter/motion.js
@@ -1,7 +1,7 @@
 const formatter = require('./formatId.js');
 
 const Movement = function (sensor) {
-  this.metricId = formatter.parse(sensor.name);
+  this.metricId = formatter.parse(`${sensor.name}${sensor.id}`);
   this.value = sensor.state.attributes.attributes.presence === true ? 1 : 0;
 };
 
