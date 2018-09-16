dc1b9a208effecaa60fef8d46258084c5ec230cf
Tomás Pérez <t@0x101.com>
2018-09-16 12:37:01 +0200

    Disabling webhooks server until its finalized.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
 
-const http = require('http');
 const taskRunner = require('./task/runner.js');
 
 taskRunner();
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.write(JSON.stringify({}));
-  res.end();
-}).listen(process.env.PORT);
