c4de594ef5e5ea8de99b6496909620a3ca8d8a09
Tomás Pérez <t@0x101.com>
2018-09-16 18:51:39 +0200

    Basic client for local oauth token fetch.

---
+++ src/health/auth.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+
+const http = require('http');
+const urlParser = require('url');
+
+const { google } = require('googleapis');
+
+const { OAuth2 } = google.auth;
+
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_OAUTH_REDIRECT_URL
+);
+
+const SCOPES = [
+  'https://www.googleapis.com/auth/fitness.activity.read',
+  'https://www.googleapis.com/auth/fitness.body.read',
+  'https://www.googleapis.com/auth/fitness.reproductive_health.read',
+  'https://www.googleapis.com/auth/fitness.blood_pressure.read'
+];
+
+http.createServer((request, response) => {
+  let body = '';
+  request.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  const parsedUrl = urlParser.parse(request.url, true);
+  const { code } = parsedUrl.query;
+
+  console.log(`Obtained OAUTH code ${code}`); // eslint-disable-line no-console
+
+  oauth2Client.getToken(code, (err, token) => {
+    console.log(`Token is ${JSON.stringify(token)}`);
+  });
+
+  request.on('end', () => {
+    console.log(body); // eslint-disable-line
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify({ request: body }));
+    response.end();
+  });
+}).listen(8001, '0.0.0.0');
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: SCOPES
+});
+
+console.log(url); // eslint-disable-line no-console
