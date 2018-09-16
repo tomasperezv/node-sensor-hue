916710da1258eee0c1339f0be2b63accb1528a9b
Tomás Pérez <t@0x101.com>
2018-09-16 23:34:12 +0200

    Using refresh tokens to persist oauth token renewal.

--- src/health/google-fitness.js
+++ src/health/google-fitness.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 
 const { google } = require('googleapis');
@@ -6,6 +7,7 @@ const fitness = google.fitness('v1');
 const { OAuth2 } = google.auth;
 
 const DATA_SOURCE_ID = 'derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm';
+let refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
 
 const fetchGoogleFitness = function (callback) {
   const localOauth2Client = new OAuth2(
@@ -13,12 +15,21 @@ const fetchGoogleFitness = function (callback) {
     process.env.GOOGLE_CLIENT_SECRET
   );
   localOauth2Client.setCredentials({
-    access_token: process.env.GOOGLE_TOKEN
+    refresh_token: refreshToken
+  });
+
+  localOauth2Client.on('tokens', (tokens) => {
+    if (tokens.refresh_token) {
+      refreshToken = tokens.refresh_token;
+      console.log(`New refresh token ${tokens.refresh_token}`);
+    } else {
+      console.log(tokens);
+    }
   });
 
   const startDate = new Date();
   const currentDate = new Date();
-  startDate.setHours(startDate.getHours() > 0 ? startDate.getHours() - 1 : 23);
+  startDate.setHours(startDate.getHours() > 0 ? startDate.getHours() - 3 : 23);
 
   const datasetId = `${startDate.getTime()}000000-${currentDate.getTime()}000000`;
 
