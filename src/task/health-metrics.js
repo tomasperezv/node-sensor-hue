711d7cf3b294c28d2aebb0a86b6bda9c78f403b7
Tomás Pérez <t@0x101.com>
2018-09-16 19:06:47 +0200

    Adding Google's fit heart bmp information metrics.

--- src/task/health-metrics.js
+++ src/task/health-metrics.js
@@ -1,21 +1,19 @@
 const googleFitness = require('../health/google-fitness.js');
 
-const huejay = require('huejay');
-
-const bridgeManager = require('../core/bridge.js');
 const apiClient = require('../api/client.js');
-const reporterFactory = require('../reporter/reporter-factory.js');
 const Constant = require('../core/constants.js');
 
 module.exports = () => {
-
   googleFitness.get()
     .then((dataPoints) => {
       const lastMetric = dataPoints[dataPoints.length - 1];
 
-      console.log(`${lastMetric.dataTypeName} ${lastMetric.value.fpVal}`); // eslint-disable-line
+      const metricId = lastMetric.dataTypeName;
+      const value = lastMetric.value[0].fpVal;
+      console.log(`${metricId} ${value}`); // eslint-disable-line
 
       if (!process.env.PUSHGATEWAY_DISABLED) {
-        apiClient.send(Constant.JOB_ID_HEALTH, lastMetric.dataTypeName, lastMetric.fpVal);
+        apiClient.send(Constant.JOB_ID_HEALTH, metricId, value);
       }
     });
+};
