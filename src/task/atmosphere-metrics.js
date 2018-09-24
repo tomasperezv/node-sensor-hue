f885a7098b753a1de40690b9d159a7f61dd9dbb0
Tomás Pérez <t@0x101.com>
2018-09-24 23:36:36 +0200

    Processing properly Promise.all results.

--- src/task/atmosphere-metrics.js
+++ src/task/atmosphere-metrics.js
@@ -3,24 +3,28 @@ const Atmosphere = require('../atmosphere/reporter.js');
 const apiClient = require('../api/client.js');
 const Constant = require('../core/constants.js');
 
+const processMetric = (result) => {
+  const { id, data } = result;
+
+  data.forEach((metric) => {
+    const metricId = `${id}${metric.name}`;
+    console.log(`${metricId} ${metric.value}`); // eslint-disable-line
+  });
+
+  if (!process.env.PUSHGATEWAY_DISABLED) {
+    data.forEach((metric) => {
+      const metricId = `${id}${metric.name}`;
+      if (metric.value !== null) {
+        console.log(`${metricId} ${metric.value}`); // eslint-disable-line
+        apiClient.send(Constant.JOB_ID_WEATHER, metricId, metric.value);
+      }
+    });
+  }
+};
+
 module.exports = () => {
   Atmosphere.get()
-    .then((result) => {
-      const { id, data } = result;
-
-      data.forEach((metric) => {
-        const metricId = `${id}${metric.name}`;
-        console.log(`${metricId} ${metric.value}`); // eslint-disable-line
-      });
-
-      if (!process.env.PUSHGATEWAY_DISABLED) {
-        data.forEach((metric) => {
-          const metricId = `${id}${metric.name}`;
-          if (metric.value !== null) {
-            console.log(`${metricId} ${metric.value}`); // eslint-disable-line
-            apiClient.send(Constant.JOB_ID_WEATHER, metricId, metric.value);
-          }
-        });
-      }
+    .then((results) => {
+      results.forEach(processMetric);
     });
 };
